3d340963f0e9411778facce4afa3c7a6fb0be418
Пименов Павел <pmpimenov@edu.hse.ru>
2018-11-28 02:34:33 +0300

    make request

--- app/src/main/java/p/testkotlinviewbinding/SecondFragment.kt
+++ app/src/main/java/p/testkotlinviewbinding/SecondFragment.kt
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_second.*
 
 
 class SecondFragment : Fragment() {
@@ -17,5 +19,14 @@ class SecondFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        FirebaseFirestore.getInstance()
+            .collection("users")
+            .document("1")
+            .get()
+            .addOnSuccessListener { doc ->
+                my_text_view.text = doc["name"] as String
+            }
+    }
 
 }
